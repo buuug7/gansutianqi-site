1eafa819f49cbda8134b2c1b7559bc3bf1944798
buuug7 <youpp@126.com>
2017-01-07 00:19:26 +0800

    update o2o farm

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,16 +13,16 @@ $(document).ready(function () {
         autoplayTimeout: 10000,
         // autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1
             },
-            768:{
+            768: {
                 items: 2
             },
-            992:{
+            992: {
                 items: 2
             },
-            1200:{
+            1200: {
                 items: 2
             }
         },
@@ -37,16 +37,16 @@ $(document).ready(function () {
         autoplayTimeout: 10000,
         // autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1
             },
-            768:{
+            768: {
                 items: 2
             },
-            992:{
+            992: {
                 items: 3
             },
-            1200:{
+            1200: {
                 items: 4
             }
         },
@@ -90,5 +90,79 @@ $(document).ready(function () {
     });
 
 
+    //
+    // index-farm
+    //
+    $(".h__img_1").backstretch(
+        ["images/farm/header.jpg"],
+        {
+            centeredX: true,
+            centeredY: true,
+            duration: 10000,
+            fade: 1500
+        }
+    );
+
+    $(".h__img_2").backstretch(
+        ["images/farm/shangji.jpg"],
+        {
+            centeredX: true,
+            centeredY: true,
+            duration: 10000,
+            fade: 1500
+        }
+    );
+
+    $('.owl2-carousel-farm-1').owlCarousel({
+        // margin: 1,
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        responsiveClass: true,
+        pagination : true,
+        nav: false,
+        responsive: {
+            1200: {
+                items: 1,
+               // autoHeight: true,
+            }
+        }
+    });
+
+    //
+    // o2o
+    //
+    $(".h__img_o2o_1").backstretch(
+        ["images/o2o/pmsbg.jpg"],
+        {
+            centeredX: true,
+            centeredY: true,
+            duration: 10000,
+            fade: 1500
+        }
+    );
+
+
+    $('.owl2-carousel-o2o-1').owlCarousel({
+        // margin: 1,
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        responsiveClass: true,
+        pagination : true,
+        nav: false,
+        responsive: {
+            1200: {
+                items: 1,
+                // autoHeight: true,
+            }
+        }
+    });
+
+
+
     App.init();
+    App.initCounter();
 });
